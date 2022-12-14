3d3bc293d60739dcddb6900951df70f0f1383a74
ntatu <ntatu@local.dev>
2022-12-15 04:11:19 +0700

    remove unused code

--- src/app/jobs/send-mail.ts
+++ src/app/jobs/send-mail.ts
@@ -11,24 +11,6 @@ let transporter = nodemailer.createTransport({
   },
 });
 
-let mailOptions = {
-  from: 'The Martec Test',
-  to: process.env.EMAIL,
-  subject: "The Martec Test - Scheduled Email",
-  html: `<p>Hello there,</p>
-  <p>This is an email scheduled from <b>The Martec Test</b></p>
-  Kind regards,<br>
-  <h4>The Martec Test</h4>`
-};
-
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    console.log("Email error application", error.message);
-  } else {
-    console.log(`${new Date().toLocaleString()} - Email sent successfully:` + info.response);
-  }
-});
-
 /**
  * Send email every hour
  * @param {string} emails 
@@ -37,14 +19,13 @@ export const sendEmail = (emails?: string[]) => {
   // Sending emails every hour
   cronJob.schedule('0 * * * * *', () => {
     let mailOptions = {
-      from: 'EMAIL NODE CRON APP',
+      from: 'The Martec Test',
       to: emails ?? config.mailing.recipients,
-      subject: "Scheduled Email",
+      subject: "The Martec Test - Scheduled Email",
       html: `<p>Hello there,</p>
-      <p> You have an email scheduled from <b>EMAIL NODE CRON APP</b> </p>
-      <p>Keep learning👌👌</p>
-      Kind regards, <br>
-      <h4> EMAIL NODE CRON APP</h4>`
+      <p>This is an email scheduled from <b>The Martec Test</b></p>
+      Kind regards,<br>
+      <h4>The Martec Test</h4>`
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
